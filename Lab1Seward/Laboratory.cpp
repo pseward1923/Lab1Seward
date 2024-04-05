62a4399986c6f5d8bc2d69717db4fa4b915e953f
pseward <pseward@rockets.utoledo.edu>
2024-04-05 12:33:58 -0400

    first commit

---
+++ Lab1Seward/Laboratory.cpp
@@ -0,0 +1,24 @@
+/*
+* Program: <First Laboratory>
+* Author:  <Peyton Seward> 
+* Date:    <1/24/24> 
+*
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main(void) {
+	float width;
+	float length;
+	float area;
+	length = 4.5;
+	width = 3.35;
+	area = length * width;
+	cout << "Rectangle Area = " << area << "\n";
+
+	cout << "Who's on First? \n";
+	return 0;
+}
